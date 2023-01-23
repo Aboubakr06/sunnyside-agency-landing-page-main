4b9395047e2e73c2a0791a11df0c4b2e69f407c3
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2023-01-23 17:53:08 +0100

    commit

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ let MenuBtn = document.querySelector(".menu-btn");
 let MenuOpen = document.querySelector(".menu");
 
 MenuBtn.addEventListener("click", function() {
-  console.log("hello");
-  MenuOpen.classList.toggle("active");
+  MenuOpen.classList.toggle("open");
 });
