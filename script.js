a5c428c47941a98d41b9153e102c0adb5f55b459
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2023-01-23 18:34:27 +0100

    commit

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ let MenuBtn = document.querySelector(".menu-btn");
 let MenuOpen = document.querySelector(".menu");
 
 MenuBtn.addEventListener("click", function() {
-  MenuOpen.classList.toggle("flex");
+  MenuOpen.classList.toggle("max-lg:hidden");
+
 });
