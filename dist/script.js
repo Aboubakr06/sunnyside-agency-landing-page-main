6c00ef501b550ab975fdd231683bc79b197e99bf
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2023-01-23 02:53:11 +0100

    commit

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+let HumbergerBt = document.querySelector(".menu-el");
+let MenuOpen = document.querySelector(".menu");
+
+HumbergerBt.addEventListener("click", function() {
+  MenuOpen.classList.toggle("show");
+});
