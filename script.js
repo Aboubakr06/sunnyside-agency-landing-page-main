51423e811a5eda424441d0a499f8d6becec3fc69
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2023-01-23 19:13:12 +0100

    commit

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let MenuBtn = document.querySelector(".menu-btn");
 let MenuOpen = document.querySelector(".menu");
 
+let ScrollBtn = document.querySelector(".scroll-btn");
+let OurTeam = document.querySelector(".team");
+
 MenuBtn.addEventListener("click", function() {
   MenuOpen.classList.toggle("max-lg:hidden");
+});
 
+ScrollBtn.addEventListener("click", function() {
+  OurTeam.scrollIntoView({ 
+    behavior: 'smooth' 
+});
 });
